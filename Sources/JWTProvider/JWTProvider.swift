ce90b5bda5a0f2f15ca7d6402bba7a0a572fa33f
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-06 11:00:53 -0600

    Created JWTProvider

--- Sources/JWTProvider/JWTProvider.swift
+++ Sources/JWTProvider/JWTProvider.swift
@@ -1,1 +1,29 @@
+import Vapor
+import JWT
 
+public class Provider: Vapor.Provider {
+    public static var repositoryName: String = "JWTProvider"
+    
+    public let serviceBuilder: (String) -> JWTService
+    
+    public init (serviceBuilder: @escaping (String) -> JWTService) {
+        self.serviceBuilder = serviceBuilder
+    }
+    
+    public func register(_ services: inout Services) throws {
+        guard let secret = Environment.get("JWT_SECRET") else {
+            throw JWTProviderError(identifier: "noSecretFound", reason: "No 'JTW_SECRET' environment variable was found")
+        }
+        
+        let jwtService = serviceBuilder(secret)
+        if let rsaService = jwtService as? RSAService {
+            services.register(rsaService)
+        } else if let hmacService = jwtService as? HMACService {
+            services.register(hmacService)
+        } else {
+            throw JWTProviderError(identifier: "unsupportedJWTService", reason: "The registered JWT service is not supported. Maybe you created a custom service?")
+        }
+    }
+    
+    public func boot(_ worker: Container) throws {}
+}
