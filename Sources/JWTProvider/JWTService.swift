4c4b937b86dd8e044a4f96fe428d0ce13d323e7c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-06 11:33:22 -0600

    Moved JWTService.sign implementation to JWTService extension

--- Sources/JWTProvider/JWTService.swift
+++ Sources/JWTProvider/JWTService.swift
@@ -5,8 +5,23 @@ import JWT
 
 public protocol JWTService: Service {
     var signer: JWTSigner { get }
+    var header: JWTHeader { get }
     
     func sign<T: JWTPayload>(_ payload: T)throws -> String
+}
+
+extension JWTService {
+    public func sign<T>(_ payload: T) throws -> String where T : JWTPayload {
+        var jwt = JWT.init(header: self.header, payload: payload)
+        let data = try signer.sign(&jwt)
+        guard let token = String(data: data, encoding: .utf8) else {
+            throw JWTProviderError(
+                identifier: "tokenEncodingFailed",
+                reason: "Converting access token data to a string failed with UTF-8 encoding"
+            )
+        }
+        return token
+    }
 }
 
 public final class RSAService: JWTService {
@@ -25,18 +40,6 @@ public final class RSAService: JWTService {
         self.signer = signerBuilder(key)
         self.header = header
     }
-    
-    public func sign<T>(_ payload: T) throws -> String where T : JWTPayload {
-        var jwt = JWT.init(header: self.header, payload: payload)
-        let data = try signer.sign(&jwt)
-        guard let token = String(data: data, encoding: .utf8) else {
-            throw JWTProviderError(
-                identifier: "tokenEncodingFailed",
-                reason: "Converting access token data to a string failed with UTF-8 encoding"
-            )
-        }
-        return token
-    }
 }
 
 public final class HMACService: JWTService {
@@ -53,18 +56,6 @@ public final class HMACService: JWTService {
     public init(secret: String, header: JWTHeader, signerBuilder: (Data) -> JWTSigner = JWTSigner.hs256) {
         self.signer = signerBuilder(Data(secret.utf8))
         self.header = header
-    }
-
-    public func sign<T>(_ payload: T) throws -> String where T : JWTPayload {
-        var jwt = JWT.init(header: self.header, payload: payload)
-        let data = try signer.sign(&jwt)
-        guard let token = String(data: data, encoding: .utf8) else {
-            throw JWTProviderError(
-                identifier: "tokenEncodingFailed",
-                reason: "Converting access token data to a string failed with UTF-8 encoding"
-            )
-        }
-        return token
     }
 }
 
