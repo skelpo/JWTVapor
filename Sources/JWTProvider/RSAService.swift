9c56936e01d6f82e41f99bde19327eb1139213da
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-03-28 10:27:33 -0500

    Updated RSAService inits for Crypto RC3

--- Sources/JWTProvider/RSAService.swift
+++ Sources/JWTProvider/RSAService.swift
@@ -6,15 +6,15 @@ public final class RSAService: JWTService {
     public let signer: JWTSigner
     public let header: JWTHeader
     
-    public init(secret: String, header: JWTHeader, bits: Int, type: RSAKeyType = .private, algorithm: RSAHashAlgorithm = .sha256) {
-        let key = RSAKey(bits: bits, type: type, data: Data(secret.utf8))
-        let rsa = RSA(hashAlgorithm: algorithm, key: key)
+    public init(secret: String, header: JWTHeader, type: RSAKeyType = .private, algorithm: DigestAlgorithm = .sha256)throws {
+        let key = try RSAKey.private(pem: secret)
+        let rsa = RSA(digestAlgorithm: algorithm, key: key)
         self.signer = JWTSigner(algorithm: rsa)
         self.header = header
     }
     
-    public init(secret: String, header: JWTHeader, keyBuilder: (Data) -> RSAKey, signerBuilder: (RSAKey) -> JWTSigner = JWTSigner.rs256) {
-        let key = keyBuilder(Data(secret.utf8))
+    public init(secret: String, header: JWTHeader, keyBuilder: (LosslessDataConvertible) -> RSAKey, signerBuilder: (RSAKey) -> JWTSigner = JWTSigner.rs256) {
+        let key = keyBuilder(secret)
         self.signer = signerBuilder(key)
         self.header = header
     }
