536494dd0e597a253782d671cfa20e5e4b2fd02b
Vibin Nair <Vibin.Nair@gmail.com>
2018-07-31 20:13:12 +0530

    JWKS:vnair - Adding the JWKS functionality.

---
+++ Sources/JWTVapor/JWKSKeys.swift
@@ -0,0 +1,17 @@
+import Foundation
+import Vapor
+
+public struct JWKSKeys: Content {
+    let keys: [JWKSKey]
+}
+
+public struct JWKSKey: Content {
+    let kty: String
+    let use: String
+    let kid: String
+    let x5t: String
+    let n: String
+    let e: String
+    let issuer: String?
+    let x5c: [String]
+}
--- Tests/JWTVaporTests/JWTProviderTests.swift
+++ Tests/JWTVaporTests/JWTProviderTests.swift
@@ -1,6 +1,20 @@
 import XCTest
-@testable import JWTProvider
+import Vapor
+import JWTVapor
 
-final class JWTVaporTests: XCTestCase {
-    static var allTests: [(String, (JWTVaporTests) -> ()throws -> ())] = []
+final class JWTProviderTests: XCTestCase {
+    static let allTests = [("testItRegistersJWKSService", testItRegistersJWKSService)]
+    
+    func testItRegistersJWKSService() {
+        var services = Services()
+        
+        let mockJWKSConfig = JWKSConfig(jwks: "https://mockUrl.com", following: "jwks_mock_uri")
+        services.register(mockJWKSConfig)
+        try? services.register(JWTProvider(RSAService.self))
+        
+        let app = try? Application(services: services)
+        let jwksService = try? app?.make(JWKSService.self)
+        
+        XCTAssertNotNil(jwksService as Any)
+    }
 }
