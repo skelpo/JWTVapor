d7db87fcd8b7e09bea5fda864425156cf66b3b06
Ralph Kuepper <ralph.kuepper@skelpo.com>
2019-12-06 14:34:31 -0500

    version fix

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta"),
-        .package(url: "https://github.com/vapor/jwt-kit.git", .branch("master")),
+        .package(url: "https://github.com/vapor/jwt-kit.git", .revision("0c7e52ab75ddfabad539a4a2a2f9fc003e7700b7")),
 //        .package(url: "https://github.com/vapor/auth.git", .branch("master"))
     ],
     targets: [
